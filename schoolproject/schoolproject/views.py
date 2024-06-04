395bbe7e619b5a06f1182c044c2c7816c5d777ce
Praful-Jain <prafuljain251001@gmail.com>
2024-06-04 18:08:51 +0530

    commit

--- schoolproject/schoolproject/views.py
+++ schoolproject/schoolproject/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-def aboutUs(request):
+def home_page(request):
+    return render(request, 'index.html')
+
+def about_us(request):
     return HttpResponse("Welcome to Jainam Academy")
+
+def course(request):
+    return HttpResponse("ds")
+
+def course_details(request, courseid):
+    return HttpResponse(courseid)
