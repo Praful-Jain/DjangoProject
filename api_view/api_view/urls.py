68e6ec7d0d040f5202b557e1475f858c76a8de1c
Praful-Jain <prafuljain251001@gmail.com>
2024-06-17 15:44:07 +0530

    improve

--- api_view/api_view/urls.py
+++ api_view/api_view/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     # path('student_api/', views.student_api),
     # path('student_api/<int:pk>', views.student_api),
     path('student_api/', views.StudentAPI.as_view()),
-    path('student_api/<int:pk>', views.StudentAPI.as_view()),
+    path('student_api/<int:pk>/', views.StudentAPI.as_view()),
 ]
